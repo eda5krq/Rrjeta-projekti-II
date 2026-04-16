2c2ea496449638637284524b7e0abe2f7c503c78
eda5krq <eda.krasniqi2@student.uni-pr.edu>
2026-04-16 19:52:29 +0200

    Add client connection handling with limit

--- ServerState.java
+++ ServerState.java
@@ -1,2 +1,18 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class ServerState {
+    private final Set<String> activeClients = new HashSet<>();
+
+    public synchronized void addClient(String clientIp) {
+        activeClients.add(clientIp);
+    }
+
+    public synchronized void removeClient(String clientIp) {
+        activeClients.remove(clientIp);
+    }
+
+    public synchronized int getActiveClientCount() {
+        return activeClients.size();
+    }
 }
